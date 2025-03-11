330c1b991dc76697a007c18f94b27fe7cefe20ae
eddie-m-m <eddie.m.menefee@gmail.com>
2025-03-11 09:12:02 -0700

    First iteration: print BBC headlines

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+)
 
-type Rss struct {
+type RSS struct {
 	XMLName xml.Name `xml:"rss"`
 	Channel Channel  `xml:"channel"`
 }
@@ -18,7 +23,33 @@ type Item struct {
 	Title       string `xml:"title"`
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
-	PubDate     string `xml:"pubdate"`
+	PubDate     string `xml:"pubDate"`
 }
 
-func main() {}
+func main() {
+
+	url := "https://feeds.bbci.co.uk/news/world/rss.xml"
+
+	bbcFeed, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error in fetching url: %v", err)
+	}
+	defer bbcFeed.Body.Close()
+
+	body, err := io.ReadAll(bbcFeed.Body)
+	if err != nil {
+		fmt.Println("error while reading feed: %v", err)
+	}
+
+	var rss RSS
+	err = xml.Unmarshal(body, &rss)
+	if err != nil {
+		fmt.Println("error while parsing rss: %v", err)
+	}
+
+	fmt.Println("Feed: ", rss.Channel.Title)
+	for _, item := range rss.Channel.Items {
+		fmt.Println("Item: ", item.Title)
+	}
+
+}
